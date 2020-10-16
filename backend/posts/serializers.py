44b7e8e6fde63b00e67c4d8cad36c2ddc14cc0ec
Romulo Nunes de Menezes Roriz <rnmroriz@gmail.com>
2020-10-16 11:19:31 +0200

    update frontend

--- backend/posts/serializers.py
+++ backend/posts/serializers.py
@@ -10,15 +10,12 @@ class PostSerializer(serializers.ModelSerializer):
     def get_likes_counter(self, post):
         return post.liked_by.all().count()
 
-    class Meta:
-        
-        author = LessInfoUserSerializer
+    author = LessInfoUserSerializer(read_only=True)
 
+    class Meta:
         model = Post
         fields = '__all__'
         read_only_fields = ['liked_by', 'author', 'like_count']
-
-        
 
 
 # class PostLessInfoSerializer(serializers.ModelSerializer):
